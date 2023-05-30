f6c02f9c32fb50b0bd3d3481c1afaf08e8eff680
Mirro0w0 <micro4399@gmail.com>
2023-05-30 22:36:21 +0800

    Search and Edit features
    1. AFF: for finding existing charts from table
    
    2. rank function for edit charts

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Website that plans to store Arcaea Charts informations.
 Right now I am working on storing all input data to a database by connecting to a database...
 So it is not done
 
-Finished: Inputting data, Calculating Chart potentials, Storing/ Listing charts by Chart potentials
+Finished: Inputting data, Calculating Chart potentials, Storing/ Listing charts by Chart potentials, Searching existing charts
 
-**Working On: Database Connection, Recent 10 mechanics
+**Working On: Editing charts from table, Database Connection, Recent 10 mechanics
 
-Future plan: Listing Best 30/ Recent 10 from Recent 30, Trie for Chart searching, HashMap for repetitive Charts...
+Future plan: Listing Best 30/ Recent 10 from Recent 30, HashMap for repetitive Charts...
 
-If you know at Javascript or an Arcaea player, I really need your help because this is my first time making my own project!! :D
+If you know Javascript or an Arcaea player, I really need your help because this is my first time making my own project!! :D
 (This is also my first time using Javascript and I want to know more logics)
--- buttons.js
+++ buttons.js
@@ -19,6 +19,7 @@ document.getElementById('FR').onclick = function()
     
 }
 
+//TC
 document.getElementById('TC').onclick = function()
 {
     if(pmset)
@@ -29,6 +30,7 @@ document.getElementById('TC').onclick = function()
     }
 }
 
+//TL
 document.getElementById('TL').onclick = function()
 {
     if(pmset)
@@ -39,13 +41,13 @@ document.getElementById('TL').onclick = function()
     }
 }
 
-document.getElementById('delete').onclick = function(){
-    console.log('10.888888888888');
-    let ans = String(10.888888888);
-    ans = ans.substring(0,5);
-    console.log(ans - 2);
-    
-}
+// random shit
+// document.getElementById('delete').onclick = function(){
+//     console.log('10.888888888888');
+//     let ans = String(10.888888888);
+//     ans = ans.substring(0,5);
+//     console.log(ans - 2);   
+// }
 
 document.getElementById('EX+').onclick = function(){
     if(!pmset)
@@ -63,4 +65,44 @@ document.getElementById('AAorBelow').onclick = function(){
     if(!pmset)
     {let dscore = document.getElementById('Score');
     dscore.value = 9700000;}
+}
+
+//Allows user to get the chart name faster for editing existing charts
+document.getElementById('SearchChart').onclick = function(){
+    let element = document.getElementById('fillchart');
+    let name = element.value;
+    let list = []; 
+    let chartrank = 0; //return the idx of that element in all[i] for edit
+
+    for (let i = 0; i < all.length; i++) //To obtain similar chart name
+    {
+        if (all[i].chartn.substring(0,name.length) == String(name)) 
+        {
+            list.push(all[i]);
+            chartrank = i + 1;
+        }
+    }
+
+    if(list.length == 1) //Only one output
+    {
+        let dchart = document.getElementById('ChartName');
+        let dchartconst = document.getElementById('ChartConst');
+        let dscore = document.getElementById('Score');
+        let drank = document.getElementById('ranker');
+
+        dchart.value = list[0].chartn;
+        dchartconst.value = list[0].chartconst;
+        dscore.value = list[0].score;
+        drank.value = chartrank;
+    }
+    else if(list.length == 0) alert("No result");
+    else //Multiple output
+    {
+        let popup = ``;
+        for (let i = 0; i < list.length; i++) 
+        {
+            popup += list[i] + `\n`;
+        }
+        alert(popup);
+    }
 }
